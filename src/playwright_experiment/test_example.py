c3ee295e60ca43b4d0f4d84200953d29db14945a
gota <gotalab555@gmail.com>
2024-12-04 23:12:39 +0900

    initial commit

---
+++ src/playwright_experiment/test_example.py
@@ -0,0 +1,18 @@
+import asyncio
+from playwright.async_api import async_playwright
+
+async def main():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch()
+        page = await browser.new_page()
+        await page.goto("https://ariel.jp/ja-jp")
+        await page.wait_for_load_state("networkidle")
+        await page.screenshot(path="screenshot.png", full_page=True)
+        print(await page.title())
+        await page.emulate_media(media="screen")
+        await page.pdf(path="page.pdf")
+        # print(await page.content())
+        await browser.close()
+
+if __name__ == "__main__":
+    asyncio.run(main())
